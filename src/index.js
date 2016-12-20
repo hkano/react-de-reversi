500970bf19357479b3588997fe6acb8ed2a23404
Hideaki Kano <kano.hideaki@gmail.com>
2016-12-20 11:31:54 +0900

    copy Tic Tac Toe.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Game from './Game';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('container')
 );
